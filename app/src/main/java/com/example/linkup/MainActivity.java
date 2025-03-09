70a8c8291173c05b56b99d31523ed7a1da1949f0
CheungTakSum <fypcomphkbu23203129@gmail.com>
2025-03-10 02:41:25 +0800

    Login Screen Layout

--- app/src/main/java/com/example/linkup/MainActivity.java
+++ app/src/main/java/com/example/linkup/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.linkup;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -8,11 +9,24 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
 
+public class MainActivity extends AppCompatActivity {
+    // Firebase features
+    FirebaseAuth auth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //[START Firebase configuration - get a object]
+        auth = FirebaseAuth.getInstance();
+        //[END configuration]
+
+        // Check if the user is logged in
+        if (auth.getCurrentUser() == null) {
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(intent);
+            finish();
+        }
     }
 }
