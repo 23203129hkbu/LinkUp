d6a42f867585a2a18f4e1bd1493248a70e415f0f
CheungTakSum <fypcomphkbu23203129@gmail.com>
2025-03-23 20:26:00 +0800

    Create Post Layout

--- app/src/main/java/com/example/linkup/Fragment/HomeFragment.java
+++ app/src/main/java/com/example/linkup/Fragment/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.example.linkup.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,19 +15,51 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.fragment.app.Fragment;
 
+import com.example.linkup.HomeOperation.CreatePost;
 import com.example.linkup.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class HomeFragment extends Fragment {
     View view;
+    // layout object
+    FloatingActionButton btnAdd;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         view = inflater.inflate(R.layout.activity_home_fragment, container, false);
+        // [START gain layout objects]
+        btnAdd = view.findViewById(R.id.btnAdd);
+        // [END gain]
 
+        // [START layout component function]
+        // Switch the screen - Update Profile
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateUI("Create");
+            }
+        });
+        // [END layout component function]
 
         // this line must be finalized
         return view;
     }
+    // [START Method]
+    // handling UI update
+    private void updateUI(String screen) {
+        Intent intent = null;
+        if (screen.equals("Setting")) {
+           // intent = new Intent(getContext(), SettingActivity.class);
+        } else if (screen.equals("Update")) {
+            // intent = new Intent(getContext(), UpdateProfile.class);
+        } else if (screen.equals("Create")) {
+            intent = new Intent(getContext(), CreatePost.class);
+        }
+        if (intent != null) {
+            startActivity(intent);
+        }
+    }
+    // [END Method]
 }
---
+++ app/src/main/java/com/example/linkup/HomeOperation/CreatePost.java
@@ -0,0 +1,22 @@
+package com.example.linkup.HomeOperation;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.linkup.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class CreatePost extends AppCompatActivity {
+    // layout object
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_post);
+
+    }
+}
